8650425fd938175378bf5bdee69788e1c1e7251a
pcw216 <pwinters@rallydev.com>
2013-08-16 23:00:02 -0400

    linking and tabs with ui-router state

--- app/scripts/controllers/mailbox/main.js
+++ app/scripts/controllers/mailbox/main.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('liam')
-  .controller('MailboxCtrl', function ($scope) {
-    
+  .controller('MailboxCtrl', function ($scope, $state, $stateParams) {
+    $scope.$state = $state;
+
+    $scope.isFolder = function(folder){
+    	var is = false
+    	if(folder == 'compose'){
+    		is = $state.is('mailbox.compose');
+    	} else {
+    		is = $state.is('mailbox.folder') && $stateParams.folder == folder;
+    	}
+    	return is;
+    };
+    $scope.goFolder = function(folder){
+    	if(folder=='compose'){
+    		$state.transitionTo('mailbox.compose', {}, { location: true, inherit: true, relative: $state.$current });
+    	} else {
+			$state.transitionTo('mailbox.folder', {folder: 'inbox'}, { location: true, inherit: true, relative: $state.$current })
+		}
+    };
+    $scope.state = function(s){
+    	debugger;
+    	var is = $state.is(s);
+    	return is;
+    }
   });
