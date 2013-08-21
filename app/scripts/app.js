852403c61c565ba88d6858ffb565dbb9f37b86ae
pcw216 <pwinters@rallydev.com>
2013-08-20 21:38:51 -0400

    really rough working oauth2 with google

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,34 +3,6 @@
 var app = angular.module
 
 angular.module('liam', ['ui.compat', 'angular-table', 'ngSanitize', 'ngCookies'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    
-    // For any unmatched url, send to /route1
-    $urlRouterProvider.otherwise("/m/inbox") 
-    $urlRouterProvider.when('/m', '/m/inbox');
-    
-    // Now set up the states
-    $stateProvider
-      .state('login', {
-        url: "/a/login",
-        templateUrl: "views/account/login.html",
-        access: {
-          public: true
-        }      
-      })
-      .state('mailbox', {
-        url: "/m",
-        templateUrl: "views/mailbox/main.html"
-      })
-        .state('mailbox.compose', {
-          url: "/compose",
-          templateUrl: "views/mailbox/compose.html"
-        })
-        .state('mailbox.folder', {
-          url: "/:folder",
-          templateUrl: "views/mailbox/folder.html"          
-        })
-  })
   .factory('openpgp', function(){
     openpgp.init();
     return openpgp;
@@ -89,8 +61,70 @@ angular.module('liam', ['ui.compat', 'angular-table', 'ngSanitize', 'ngCookies']
             username: null
           });
           callback(null, currentUser);
+      },
+      google: {
+        clientId: '1066133385516.apps.googleusercontent.com',
+        scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
+      },
+      googleAuth: function(callback){
+        // var url = 'https://accounts.google.com/o/oauth2/auth?'+
+        //   'client_id='+encodeURIComponent(this.google.clientId)+
+        //   '&scope='+encodeURIComponent(this.google.scope)+
+        //   '&state='+encodeURIComponent(state)+
+        //   '&response_type=token'+
+        //   '&redirect_uri='+encodeURIComponent(this.google.redirectUri);
+        // window.location.href = url;
+
+        gapi.auth.authorize({
+          client_id: this.google.clientId,
+          scope: this.google.scope,
+          immediate: false,
+          authuser: -1
+        }, callback)
       }
     }
+  })
+  .config(function ($stateProvider, $urlRouterProvider, $routeProvider) {
+    
+    // For any unmatched url, send to /route1
+    $urlRouterProvider.when('/m', '/m/inbox');
+    $urlRouterProvider.when('access_token', ['$location', function($location){
+      debugger
+
+    }]);
+    $urlRouterProvider.otherwise("/m/inbox");
+    
+    // Now set up the states
+    $stateProvider
+      .state('root', {
+        url: "access_token",
+        access: {
+          public: true
+        },
+        controller: function(){
+          debugger
+        }
+      })
+      .state('login', {
+        url: "/a/login",
+        templateUrl: "views/account/login.html",
+        access: {
+          public: true
+        }      
+      })
+      .state('mailbox', {
+        url: "/m",
+        templateUrl: "views/mailbox/main.html"
+      })
+        .state('mailbox.compose', {
+          url: "/compose",
+          templateUrl: "views/mailbox/compose.html"
+        })
+        .state('mailbox.folder', {
+          url: "/:folder",
+          templateUrl: "views/mailbox/folder.html"          
+        })
+
   })
   .run(function($rootScope, $state, $timeout, $auth){
     $rootScope.isLoggedIn = $auth.isLoggedIn;
--- app/scripts/controllers/account/login.coffee
+++ app/scripts/controllers/account/login.coffee
@@ -1,14 +1,16 @@
 module = angular.module("liam")
 
 class LoginController
-	constructor: (@$scope, @$state, @$auth) ->
+	constructor: (@$scope, @$location, @$state, @$auth) ->
 		@bindScope()
-
 	
 	bindScope: ->
 		@$scope.user = {}
 		@$scope.login = @login
 		@$scope.register = @register
+
+		@$scope.handleGoogleAuth = @handleGoogleAuth
+		@$scope.googleAuth = @googleAuth
 
 	login: () =>
 		@$scope.user.firstName = @$scope.user.username
@@ -19,8 +21,28 @@ class LoginController
 		@$auth.register @$scope.user, () =>
 			@$state.transitionTo 'mailbox'
 
+	handleGoogleAuth: (authResult) =>
+		debugger
+		console.log authResult
+		gapi.auth.setToken(authResult)
+		gapi.client.load 'oauth2', 'v2', () =>
+			request = gapi.client.oauth2.userinfo.get().execute (user) =>
+				theUser = {
+					lastName: user.family_name
+					firstName: user.given_name
+					username: user.email
+				}
+				@$auth.login theUser, () =>
+					@$state.transitionTo 'mailbox'
+
+	googleAuth: () =>
+		@$auth.googleAuth(@$scope.handleGoogleAuth)
+		#window.location.href = 'https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&state=%2Fprofile&redirect_uri=https%3A%2F%2Foauth2-login-demo.appspot.com%2Foauthcallback&response_type=token&client_id=812741506391.apps.googleusercontent.com'
+		#@gapi.auth.authorize({client_id: @gapi.clientId, scope: @gapi.scopes, immediate: false}, @$scope.handleGoogleAuth);
+
 LoginController.$inject = [
 	"$scope"
+	"$location"
 	"$state"
 	"$auth"
 	]
